a0f244899762830444f7e1d43b0eb8e897f64b8e
Oskar Kellermann <oskar@kellermann.team>
2023-12-13 01:10:23 +0100

    fitted exercisebutonnto design and added real data

--- lib/pages/workout/workoutPage/widgets/exerciseButton.dart
+++ lib/pages/workout/workoutPage/widgets/exerciseButton.dart
@@ -1,10 +1,9 @@
-import 'package:flexify/data/exerciseModels.dart';
-import 'package:flexify/pages/workout/workoutPage/setPage/setsPage.dart';
-import 'package:flexify/pages/workout/workoutPage/widgets/exerciseButton/exerciseButtonContainer.dart';
-import 'package:flexify/pages/workout/workoutPage/widgets/exerciseButton/exerciseButtonText.dart';
-import 'package:flexify/pages/workout/workoutPage/widgets/exerciseButton/muscleCooldown.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+
+import 'package:flexify/data/exerciseModels.dart';
+import 'package:flexify/pages/workout/workoutPage/setPage/setsPage.dart';
+import 'package:flexify/pages/workout/workoutPage/widgets/muscleCooldown.dart';
 
 class ExerciseButton extends StatefulWidget {
   const ExerciseButton({
@@ -21,6 +20,68 @@ class ExerciseButton extends StatefulWidget {
 }
 
 class _ExerciseButtonState extends State<ExerciseButton> {
+  List<Set> sets = [];
+  getData() async {
+    sets = (await Save.setSetIfNull())
+        .map((e) => Set.fromStringToObject(e))
+        .where((e) => e.exerciseName == widget.exercise.name)
+        .toList();
+
+    setState(() {});
+  }
+
+  Set getMaxSet(List<Set> sets) {
+    if (sets.isEmpty) {
+      return Set(
+        setId: -1,
+        date: DateTime.now(),
+        exerciseName: widget.exercise.name,
+        reps: 0,
+        weight: 0,
+      );
+    }
+
+    Set pr = sets[0];
+    for (int i = 1; i < sets.length; i++) {
+      if (sets[i].weight * sets[i].reps > pr.weight * pr.reps) {
+        pr = sets[i];
+      }
+    }
+    return pr;
+  }
+
+  String zeroBefore(int num) {
+    if (num <= 9) {
+      return '0$num';
+    }
+    return '$num';
+  }
+
+  String dateString(DateTime date) {
+    String month = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec'
+    ][date.month - 1];
+
+    return '$month ${date.day}';
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -34,17 +95,57 @@ class _ExerciseButtonState extends State<ExerciseButton> {
           type: PageTransitionType.fade,
         ),
       ),
-      child: ExerciseButtonContainer(
-        content: Padding(
-          padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.1),
-          child: const Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              ExerciseButtonText(),
-              MuscleCooldown(),
-            ],
-          ),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.9,
+        height: MediaQuery.of(context).size.height * 0.2,
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(35),
+          color: Theme.of(context).colorScheme.background,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.exercise.name,
+                  style: TextStyle(
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'best set: ${getMaxSet(sets).reps} x ${getMaxSet(sets).weight}kg',
+                  style: TextStyle(
+                    color: Theme.of(context)
+                        .scaffoldBackgroundColor
+                        .withOpacity(0.4),
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+                // LAST TRAIN DATE
+                sets.isNotEmpty
+                    ? Text(
+                        'last set: ${dateString(sets.last.date)} at ${zeroBefore(sets.last.date.hour)}:${zeroBefore(sets.last.date.minute)}',
+                        style: TextStyle(
+                          color: Theme.of(context)
+                              .scaffoldBackgroundColor
+                              .withOpacity(0.4),
+                        ),
+                      )
+                    : const SizedBox(),
+              ],
+            ),
+            MuscleCooldown(
+              sets: sets,
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/pages/workout/workoutPage/widgets/muscleCooldown.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+import 'package:flexify/data/exerciseModels.dart';
+
+class MuscleCooldown extends StatelessWidget {
+  MuscleCooldown({
+    super.key,
+    required this.sets,
+  });
+
+  final List<Set> sets;
+
+  String loadString = '';
+  double recoverValue = 0.0;
+  Color recoverColor = Colors.red;
+
+  getData(BuildContext context) {
+    if (sets.isEmpty) {
+      recoverValue = 1;
+      loadString = 'let\'s go!';
+      recoverColor = Theme.of(context).colorScheme.primary;
+      return;
+    }
+
+    // CALC recoverValue
+    recoverValue =
+        (72 / 100) * sets.last.date.difference(DateTime.now()).inHours;
+
+    if (recoverValue <= 0.6) {
+      loadString = 'recovering';
+      recoverColor = Theme.of(context).colorScheme.error;
+    }
+
+    if (recoverValue > 0.6) {
+      loadString = 'nearly done';
+      recoverColor = Theme.of(context).colorScheme.onPrimary;
+    }
+
+    if (recoverValue == 1) {
+      loadString = 'done';
+      recoverColor = Theme.of(context).colorScheme.primary;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getData(context);
+    return Container(
+      margin: const EdgeInsets.all(20),
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Transform.scale(
+            scale: 2,
+            child: CircularProgressIndicator(
+              value: 1,
+              color: Theme.of(context).colorScheme.surface.withOpacity(0.1),
+              strokeCap: StrokeCap.round,
+              strokeAlign: BorderSide.strokeAlignOutside,
+            ),
+          ),
+          Transform.scale(
+            scale: 2,
+            child: CircularProgressIndicator(
+              value: recoverValue,
+              color: recoverColor,
+              strokeCap: StrokeCap.round,
+              strokeAlign: BorderSide.strokeAlignOutside,
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                loadString,
+                style: TextStyle(
+                  color: recoverColor,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 10,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
