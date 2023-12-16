c2dbdaf661bbe92869d569cb8a2b0fb8d49b230a
Miintaged <quadrilateralpusher@gmail.com>
2023-12-16 14:40:46 +0100

    Global Variables

---
+++ lib/data/dummyExercises.dart
@@ -0,0 +1,64 @@
+import 'package:flexify/data/exerciseModels.dart';
+
+List<Exercise> gymExercises = [
+  Exercise(
+    name: "Barbell Squats",
+    type: "Squat",
+    affectedMuscle: "Quads",
+    equipment: "Barbell",
+  ),
+  Exercise(
+    name: "Deadlifts",
+    type: "Compound",
+    affectedMuscle: "Hamstrings",
+    equipment: "Barbell",
+  ),
+  Exercise(
+    name: "Bench Press",
+    type: "Compound",
+    affectedMuscle: "Chest",
+    equipment: "Barbell",
+  ),
+  Exercise(
+    name: "Dumbbell Lunges",
+    type: "Lunge",
+    affectedMuscle: "Quads",
+    equipment: "Dumbbells",
+  ),
+  Exercise(
+    name: "Pull-ups",
+    type: "Compound",
+    affectedMuscle: "Back",
+    equipment: "Pull-up bar",
+  ),
+  Exercise(
+    name: "Shoulder Press",
+    type: "Isolation",
+    affectedMuscle: "Shoulders",
+    equipment: "Dumbbells",
+  ),
+  Exercise(
+    name: "Tricep Dips",
+    type: "Isolation",
+    affectedMuscle: "Triceps",
+    equipment: "Parallel bars",
+  ),
+  Exercise(
+    name: "Leg Press",
+    type: "Compound",
+    affectedMuscle: "Legs",
+    equipment: "Leg press machine",
+  ),
+  Exercise(
+    name: "Chest Flyes",
+    type: "Isolation",
+    affectedMuscle: "Chest",
+    equipment: "Cable machine",
+  ),
+  Exercise(
+    name: "Bicep Curls",
+    type: "Isolation",
+    affectedMuscle: "Biceps",
+    equipment: "Dumbbells",
+  ),
+];
---
+++ lib/data/globalVariables.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+double borderRadius = 35;
+double containerWidthFactor = 0.95;
+double containerPadding = 30;
+
+IconData arrow = Icons.arrow_back_rounded;
+
