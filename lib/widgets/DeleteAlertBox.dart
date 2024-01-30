68b50c4a548f95c0b626eadf8ff2383f6e64cad8
Oskar Kellermann <oskar@kellermann.team>
2024-01-30 12:00:09 +0100

    changed backgroundcolor of DeleteAlertbox to better backgrounbdcolor

--- lib/widgets/DeleteAlertBox.dart
+++ lib/widgets/DeleteAlertBox.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class DeleteAlertDialog extends StatelessWidget {
   DeleteAlertDialog({
     super.key,
@@ -10,19 +11,43 @@ class DeleteAlertDialog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      backgroundColor: Theme.of(context).colorScheme.background,
-      content: Padding(
-        padding: const EdgeInsets.only(top: 8),
-        child: Text(
-          'Do you want to delete this set?',
-          style: TextStyle(
-            fontSize: MediaQuery.of(context).size.width * 0.04,
-            color: Theme.of(context).colorScheme.surface,
+    return Theme(
+      data: ThemeData(
+        fontFamily: 'JosefinSans',
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme(
+          brightness: Brightness.dark,
+          primary: Colors.transparent,
+          onPrimary: Theme.of(context).colorScheme.primary,
+          secondary: Colors.red,
+          onSecondary: Colors.red,
+          error: Colors.red,
+          onError: Colors.red,
+          background: Colors.green,
+          onBackground: Colors.transparent,
+          surface: Theme.of(context).colorScheme.background,
+          onSurface: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: Theme.of(context).colorScheme.primary,
           ),
         ),
       ),
-      actions: actions,
+      child: AlertDialog(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        content: Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            'Do you want to delete this set?',
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.width * 0.04,
+              color: Theme.of(context).colorScheme.surface,
+            ),
+          ),
+        ),
+        actions: actions,
+      ),
     );
   }
 }
