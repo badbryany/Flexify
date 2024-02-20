3d19f1559db696c822f739aa29d8ace1f55bfe2a
Arthur <arthur@kellermann.team>
2024-02-20 20:00:44 +0100

    food page CircularPercentIndicator finished

--- lib/pages/food/dashboardFood.dart
+++ lib/pages/food/dashboardFood.dart
@@ -1,9 +1,7 @@
-import 'package:flexify/pages/food/meal/dinner.dart';
-import 'package:flexify/pages/food/meal/lunch.dart';
 import 'package:flutter/material.dart';
-import 'package:flexify/pages/food/widget/background.dart';
 import 'package:flexify/data/globalVariables.dart';
-import 'package:flexify/pages/food/meal/breakfast.dart';
+import 'package:flexify/data/globalVariables.dart' as global;
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class DashboardFood extends StatefulWidget {
   const DashboardFood({super.key});
@@ -13,112 +11,109 @@ class DashboardFood extends StatefulWidget {
 }
 
 class DashboardFoodState extends State<DashboardFood> {
-  Widget? refresh;
-  int pagecounter = 1;
-  final controller = PageController(initialPage: 1);
-
-  List<Map<String, dynamic>> dailyPlan = [
-    {
-      'meal': 'Breakfast',
-      'calories': 0.0,
-      'foodEaten': [],
-    },
-    {
-      'meal': 'Lunch',
-      'calories': 0.0,
-      'foodEaten': [],
-    },
-    {
-      'meal': 'Dinner',
-      'calories': 0.0,
-      'foodEaten': [],
-    },
-  ];
+  double maxCalories = 2000;
+  double eatenCalories = 200;
 
   @override
   Widget build(BuildContext context) {
-    controller.addListener(
-      () {
-        if (controller.page == 0) {
-          refresh = const Breakfast();
-        }
-        if (controller.page == 1) {
-          refresh = const Lunch();
-        }
-        if (controller.page == 2) {
-          refresh = const Dinner();
-        }
-        setState(() {});
-      },
-    );
+    double leftCalories = eatenCalories / maxCalories;
+    Color progressColor = Theme.of(context).colorScheme.primary;
+
+    if (leftCalories > 1.0) {
+      leftCalories = 1.0;
+      progressColor = Theme.of(context).colorScheme.error;
+    }
 
     return Container(
       padding: EdgeInsets.all(containerPadding),
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Background(
-            child: Center(
+      child: Container(
+        padding: EdgeInsets.only(top: containerPadding),
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.background,
+            borderRadius: BorderRadius.circular(borderRadius),
+            boxShadow: [global.lightShadow(context)]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(bottom: containerPadding),
               child: Text(
-                'Daily Plan:',
+                'Today',
                 style: TextStyle(
                   color: Theme.of(context).colorScheme.onBackground,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ),
-          ),
-          Container(
-            padding: EdgeInsets.only(top: containerPadding),
-            width: MediaQuery.of(context).size.width - containerPadding * 2,
-            height: MediaQuery.of(context).size.height * 0.1,
-            child: PageView(
-              controller: controller,
-              children: dailyPlan.map((e) {
-                return Background(
-                  child: Container(
-                    alignment: Alignment.centerLeft,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.only(right: containerPadding * 0.5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         Text(
-                          e['meal'],
+                          'Eaten Calories: ',
                           style: TextStyle(
                             color: Theme.of(context).colorScheme.onBackground,
                           ),
                         ),
-                        Row(
-                          children: [
-                            Text(
-                              'calories: ',
-                              style: TextStyle(
-                                color:
-                                    Theme.of(context).colorScheme.onBackground,
-                              ),
-                            ),
-                            Text(
-                              e['calories'].toString(),
-                              style: TextStyle(
-                                color:
-                                    Theme.of(context).colorScheme.onBackground,
-                              ),
-                            ),
-                          ],
+                        Text(
+                          eatenCalories.toString(),
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
                         ),
                       ],
                     ),
                   ),
-                );
-              }).toList(),
-            ),
-          ),
-          AnimatedSwitcher(
-            duration: standardAnimationDuration,
-            child: refresh,
-          )
-        ],
+                  Center(
+                    child: CircularPercentIndicator(
+                      radius: 25,
+                      lineWidth: 3,
+                      backgroundColor: Theme.of(context).canvasColor,
+                      percent: leftCalories,
+                      progressColor: progressColor,
+                      circularStrokeCap: CircularStrokeCap.round,
+                      animation: true,
+                      center: Text(
+                        '${maxCalories.toInt() - eatenCalories.toInt()}',
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: containerPadding * 0.5),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Max Calories: ',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                        ),
+                        Text(
+                          maxCalories.toString(),
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
