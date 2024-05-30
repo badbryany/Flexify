552020ec6270680e6cfd5e889afb6cddcf9cb1b6
Oskar Kellermann <oskar@kellermann.team>
2024-05-30 08:30:28 +0200

    personal plan and leadworkoutwidget design idea/first design(not good)

---
+++ lib/pages/dWorkout/widgets/WorkoutPlan.dart
@@ -0,0 +1,76 @@
+import 'package:flexify/pages/dWorkout/workoutPlanPage/workoutPlanPage.dart';
+import 'package:flexify/widgets/BounceElement.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flexify/data/globalVariables.dart' as global;
+import 'package:page_transition/page_transition.dart';
+
+class WorkoutPlan extends StatelessWidget {
+  const WorkoutPlan({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BounceElement(
+      child: GestureDetector(
+        onTap: () => Navigator.push(
+          context,
+          PageTransition(
+            child: const WorkoutPlanPage(),
+            type: PageTransitionType.rightToLeft,
+          ),
+        ),
+        child: Container(
+          padding: EdgeInsets.all(global.containerPadding),
+          decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.surface,
+            borderRadius: BorderRadius.circular(global.borderRadius),
+            boxShadow: [global.darkShadow(context)],
+            // border: Border.all(
+            //   color: Theme.of(context).colorScheme.primary.withOpacity(.35),
+            //   width: 1.5,
+            // ),
+          ),
+          width: global.width(context) * global.containerWidthFactor,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  global.gradient(
+                    Text(
+                      'Personal Plan',
+                      style: TextStyle(
+                        fontSize: global.width(context) * .065,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).focusColor,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 5),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    width: global.width(context) *
+                        global.containerWidthFactor *
+                        .7,
+                    child: Text(
+                      'Your personal recommended exercises of your regular plan',
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        color: Theme.of(context).focusColor.withOpacity(0.7),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Icon(CupertinoIcons.arrow_right),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/dWorkout/workoutPlanPage/widgets/LeadWorkout.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flexify/data/globalVariables.dart' as global;
+
+class LeadWorkout extends StatefulWidget {
+  const LeadWorkout({super.key});
+
+  @override
+  State<LeadWorkout> createState() => _LeadWorkoutState();
+}
+
+class _LeadWorkoutState extends State<LeadWorkout> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(global.borderRadius),
+        color: Theme.of(context).colorScheme.surface,
+      ),
+      width: global.containerWidth(context),
+      height: global.height(context) * .4,
+      margin: EdgeInsets.symmetric(
+        vertical: 6,
+        horizontal:
+            (global.width(context) * (1 - global.containerWidthFactor)) / 2,
+      ),
+      child: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.all(global.width(context) * .1),
+            alignment: Alignment.topLeft,
+            child: Text(
+              'next set:',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onSurface.withOpacity(.5),
+                fontSize: global.width(context) * .05,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Reverse Buttefly',
+                  style: TextStyle(
+                    fontSize: global.width(context) * .075,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    global.gradient(Text(
+                      '54kg',
+                      style: TextStyle(
+                        fontSize: global.width(context) * .07,
+                      ),
+                    )),
+                    Text(
+                      ' x 12',
+                      style: TextStyle(
+                        fontSize: global.width(context) * .07,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          global.gradient(Container(
+            margin: EdgeInsets.all(global.width(context) * .1),
+            alignment: Alignment.bottomRight,
+            child: Text('done'),
+          )),
+          Container(
+            margin: EdgeInsets.all(global.width(context) * .1),
+            alignment: Alignment.bottomLeft,
+            child: Text('timer'),
+          ),
+        ],
+      ),
+    );
+  }
+}
