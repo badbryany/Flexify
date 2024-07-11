256c88e758b30ee9faa90f094c0f8b348e26b1b6
Oskar Kellermann <oskar@kellermann.team>
2024-07-11 13:48:14 +0200

    "Your daily stats" is working now

--- lib/data/globalVariables.dart
+++ lib/data/globalVariables.dart
@@ -223,3 +223,9 @@ String shrinkNum(int number) {
 
   return 'many';
 }
+
+extension DateOnlyCompare on DateTime {
+  bool isSameDate(DateTime other) {
+    return year == other.year && month == other.month && day == other.day;
+  }
+}
